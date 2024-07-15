8a67f63ed3f68e6b421bb5208dcedf327c9fadee
vivek <viveksingh40062@gmail.com>
2024-07-15 14:45:06 +0530

    updated app.js by using useLocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Details from './components/Details';
 
 const App = () => {
+  const{search, pathname} = useLocation()
+
+
   return (
     <div className="h-screen w-screen flex">
+        {(pathname!="/" || search.length>0)&&(
+
       <Link to="/" className="text-red-300 absolute left-[17%] top-[3%]">
         Home
       </Link>
+        )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<Details />} />
